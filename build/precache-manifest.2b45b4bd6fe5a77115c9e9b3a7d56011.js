22d60f77e525b8652a7d809094bcc273c50c13f6
salemm <emmanuel@bquate.com>
2019-06-14 13:33:20 -0500

    :rocket: build done

---
+++ build/precache-manifest.2b45b4bd6fe5a77115c9e9b3a7d56011.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0746949faecaf7c7dfef4440a5ff0f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb7b6f8b1b763fbe996a",
+    "url": "/static/css/2.6a7b8a39.chunk.css"
+  },
+  {
+    "revision": "3234dbfa36d6d6110e5a",
+    "url": "/static/css/main.fd1f09aa.chunk.css"
+  },
+  {
+    "revision": "cb7b6f8b1b763fbe996a",
+    "url": "/static/js/2.9ae68721.chunk.js"
+  },
+  {
+    "revision": "3234dbfa36d6d6110e5a",
+    "url": "/static/js/main.470530d3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b4dc037cc147ff1cd7392409b6d89a22",
+    "url": "/static/media/exibit.b4dc037c.JPG"
+  }
+]);
